4cfba481373f5e43df22f4fd943bbb394685c337
Odair Pedroso <odair.junior@luizalabs.com>
2019-02-01 17:13:21 -0200

    commit jogo python

---
+++ jogo1.py
@@ -0,0 +1,9 @@
+Python 3.5.4 (v3.5.4:3f56838, Aug  8 2017, 02:17:05) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+>>> "hello world"
+'hello world'
+>>> 2**8
+256
+>>> 
